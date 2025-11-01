2d095f2b3ebc8a2950cf32d58bb891e69e709830
Bin_Roper <Bin_Roper@DESKTOP-62PPTRH>
2025-11-01 19:01:44 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppHotel.Views
+{
+    public partial class ContratacaoHospedagem : ContentPage
+    {
+        public ContratacaoHospedagem()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSobreClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert(
+                "Sobre",
+                "App modificado por Fabiano Pereira em 30 de outubro de 2025",
+                "OK");
+            
+        }
+    }
+}
